b1c8446d97b0f3fc481dccdf92e483fa219221cc
Kajal Pawar <kp668138@gmail.com>
2025-01-17 22:09:37 +0530

    Add example of type promotion to convert int to double

---
+++ TypePromotion1.java
@@ -0,0 +1,15 @@
+class demo
+{
+void display(double a)
+{
+System.out.println("Promoted value:"+a);
+}
+}
+class TypePromotion1
+{
+public static void main(String  args[])
+{
+demo d1=new demo();
+d1.display(2);
+}
+}
