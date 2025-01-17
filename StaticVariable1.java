8747e944768ef14f25f6a16ca8c3985dbcef7967
Kajal Pawar <kp668138@gmail.com>
2025-01-18 00:19:06 +0530

    Add example of static variable

---
+++ StaticVariable1.java
@@ -0,0 +1,21 @@
+class demo
+{
+public int rollno=10;
+void change()
+{
+rollno++;
+System.out.println(rollno);
+}
+}
+class StaticVariable1
+{
+public static void main(String args[])
+{
+demo d1=new demo();
+d1.change();
+demo d2=new demo();
+d2.change();
+demo d3=new demo();
+d3.change();
+}
+}
