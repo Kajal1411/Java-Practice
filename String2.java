e4efcdea07677dce3bb6057f356e214dbf03fd18
Kajal Pawar <kp668138@gmail.com>
2025-02-26 18:45:57 +0530

    Add program to convert character array to string

---
+++ String2.java
@@ -0,0 +1,10 @@
+import java.lang.String;
+class String2
+{
+public static void main(String args[])
+{
+char a[]={'k','d','n'};  //null char is optional
+String s=new String(a);
+System.out.println("String:"+s);
+}
+}
