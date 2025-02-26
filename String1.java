756840a01f4b1baa3eb0cc632348178b9a1e515d
Kajal Pawar <kp668138@gmail.com>
2025-02-26 18:44:32 +0530

    Add program to print simple string

---
+++ String1.java
@@ -0,0 +1,11 @@
+import java.lang.String;
+class String1
+{
+public static void main(String args[])
+{
+String s1="Kajal";
+String s2=new String("Pawar");
+System.out.println("First name:"+s1);
+System.out.println("Last name:"+s2);
+}
+}
