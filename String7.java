9e820069106f3ccfc9fc1da2dcac753ac0263f3d
Kajal Pawar <kp668138@gmail.com>
2025-02-27 18:46:28 +0530

    Add Program to concat 2 strings using concat() method

---
+++ String7.java
@@ -0,0 +1,12 @@
+//Program to concat 2 strings using concat() method.
+import java.lang.String;
+class String7
+{
+public static void main(String args[])
+{
+String s1="kajal";
+String s2="Pawar";
+String s3=s1.concat(s2);
+System.out.println(s3);  
+}
+}
