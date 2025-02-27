555fbb4a946ecba9b6a23c1c433b1f106bc1cb3f
Kajal Pawar <kp668138@gmail.com>
2025-02-27 18:44:56 +0530

    Add Program to concat 2 strings using + operator

---
+++ String6.java
@@ -0,0 +1,10 @@
+//Program to concat 2 strings using + operator.
+import java.lang.String;
+class String6
+{
+public static void main(String args[])
+{
+String s="KDN"+"Infotech";
+System.out.println(s);  
+}
+}
