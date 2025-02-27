a47ec963c159d9c7103146fb2b7d9611e6e1936c
Kajal Pawar <kp668138@gmail.com>
2025-02-27 18:47:58 +0530

    Add Program for substring()

---
+++ String8.java
@@ -0,0 +1,12 @@
+//Program for substring().
+import java.lang.String;
+class String8
+{
+public static void main(String args[])
+{
+String s="Sachin Tendulkar";
+System.out.println(s.substring(6));
+System.out.println(s.substring(0,6));
+
+}
+}
