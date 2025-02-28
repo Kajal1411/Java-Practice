6732aa4a4b793dc1bf931184f5b6f64b68cc55bb
Kajal Pawar <kp668138@gmail.com>
2025-02-28 23:38:05 +0530

    Add Program to convert the string into  uppercase and lowercase

---
+++ String9.java
@@ -0,0 +1,11 @@
+//Program for uppercase and lowercase.
+import java.lang.String;
+class String9
+{
+public static void main(String args[])
+{
+String s="Sachin Tendulkar";
+System.out.println(s.toUpperCase());
+System.out.println(s.toLowerCase());
+}
+}
