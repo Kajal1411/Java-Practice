9e865034a89de6818756d37795173f357d8f7387
Kajal Pawar <kp668138@gmail.com>
2025-03-01 00:05:27 +0530

    Add program for string contains() method

---
+++ String16.java
@@ -0,0 +1,14 @@
+//contains()
+
+import java.lang.String;
+import java.util.Scanner;
+class String16
+{
+public static void main(String args[])
+{
+String s="what do you know";
+System.out.println(s.contains("what"));
+System.out.println(s.contains("are"));
+System.out.println(s.contains("od"));
+}
+}
