3b6f7858d2ab611eb488ed07c983618de84833c4
Kajal Pawar <kp668138@gmail.com>
2025-02-28 23:43:48 +0530

    Add program for string startWith(),endWith() function

---
+++ String11.java
@@ -0,0 +1,13 @@
+//Program for startWith(),endWith().
+import java.lang.String;
+class String11
+{
+public static void main(String args[])
+{
+String s="Sachin";
+System.out.println(s.startsWith("S"));
+System.out.println(s.startsWith("K"));
+System.out.println(s.endsWith("n"));
+System.out.println(s.endsWith("i"));
+}
+}
