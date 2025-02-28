459d338f8883b51887f0c5231c7115319193bb9a
Kajal Pawar <kp668138@gmail.com>
2025-03-01 00:01:58 +0530

    Add program for string charAt() method

---
+++ String14.java
@@ -0,0 +1,12 @@
+//charAt()
+
+import java.lang.String;
+import java.util.Scanner;
+class String14
+{
+public static void main(String args[])
+{
+String s="kajal";
+System.out.println(s.charAt(1));
+}
+}
