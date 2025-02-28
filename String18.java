ad991360c4cae00ef5a126656890899032f9ff65
Kajal Pawar <kp668138@gmail.com>
2025-03-01 00:12:45 +0530

    Add program to convert string into bytes without using getBytes() method

---
+++ String18.java
@@ -0,0 +1,15 @@
+import java.lang.String;
+class String18
+{
+public static void main(String args[])
+{
+String s="ABCDEF";
+int i,num;
+char arr[]=s.toCharArray();
+for(i=0;i<=arr.length;i++)
+{
+num=arr[i];
+System.out.println(num);
+}
+}
+}
