f33f3f2d50f153c115c6b27f941379dce7551bfc
Kajal Pawar <kp668138@gmail.com>
2025-03-02 19:45:33 +0530

    Add program to replace only one character/substring with another charater/substring from string using replace() method

---
+++ String21.java
@@ -0,0 +1,10 @@
+import java.lang.String;
+class String21
+{
+public static void main(String args[])
+{
+String s="what you are doing";
+String s1=s.replace("are","is");
+System.out.println(s1);
+}
+}
