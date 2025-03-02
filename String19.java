5407366d25fc12049c5bd36d9e73b2e41cf50b82
Kajal Pawar <kp668138@gmail.com>
2025-03-02 19:38:20 +0530

    Add program to return starting index of given substring using indexOf() method

---
+++ String19.java
@@ -0,0 +1,11 @@
+import java.lang.String;
+import java.util.Scanner;
+class String19
+{
+public static void main(String args[])
+{
+String s="what do you know";
+int i=s.indexOf("you");
+System.out.println(i);
+}
+}
