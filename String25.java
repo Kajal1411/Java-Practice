4cd6bd437fe44c6c0f9d8ebc053c668c96c07ae4
Kajal Pawar <kp668138@gmail.com>
2025-03-03 00:16:05 +0530

    Add program for StringBuilder class

---
+++ String25.java
@@ -0,0 +1,10 @@
+import java.lang.String;
+class String25
+{
+public static void main(String args[])
+{
+StringBuilder s=new StringBuilder("kajal");
+System.out.println(s.append("pawar"));
+System.out.println(s);
+}
+}
