1e2c50ffb032f7db44110333ac833215e99d450c
Kajal Pawar <kp668138@gmail.com>
2025-03-03 00:18:03 +0530

    Add program for StringBuffer class

---
+++ String26.java
@@ -0,0 +1,10 @@
+import java.lang.String;
+class String26
+{
+public static void main(String args[])
+{
+StringBuffer s=new StringBuffer("kajal");
+System.out.println(s.append("pawar"));
+System.out.println(s);
+}
+}
