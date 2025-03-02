8a0d21e9a74cfff52aaaf6ce578e632eb6204c18
Kajal Pawar <kp668138@gmail.com>
2025-03-02 19:48:20 +0530

    Add program to replace all characters  with another charater by using replaceAll() method

---
+++ String22.java
@@ -0,0 +1,10 @@
+import java.lang.String;
+class String22
+{
+public static void main(String args[])
+{
+String s="what you are doing";
+String s1=s.replaceAll("o","e");
+System.out.println(s1);
+}
+}
