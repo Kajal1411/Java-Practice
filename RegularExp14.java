1d3f616a90ecf06106384be5484681af7b2fc416
Kajal Pawar <kp668138@gmail.com>
2025-03-06 23:31:29 +0530

    Add program to check given substring is present or not in original string using the regex

---
+++ RegularExp14.java
@@ -0,0 +1,25 @@
+import java.util.regex.*;
+import java.util.Scanner;
+class RegularExp14
+{
+public static void main(String args[])
+{
+String str;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter string:");
+str=sc.nextLine();
+
+Pattern p=Pattern.compile(str);
+Matcher m=p.matcher("KDN Infotech");
+
+if(m.find())
+{
+System.out.println("String is found");
+}
+else
+{
+System.out.println("String is not found");
+}
+
+}
+} 
