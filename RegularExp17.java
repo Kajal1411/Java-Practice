b3e593e3e40ef3efad19c2f191f326af9066d93a
Kajal Pawar <kp668138@gmail.com>
2025-03-06 23:38:19 +0530

    Add program for regex

---
+++ RegularExp17.java
@@ -0,0 +1,11 @@
+import java.util.regex.*;
+class RegularExp17
+{
+public static void main(String args[])
+{
+boolean b1=Pattern.matches(".s","as");  
+System.out.println(b1);
+boolean b2=Pattern.matches(".k","as");  
+System.out.println(b2);
+}
+} 
