f54da3053db94ab15c89b204450ef7081d30ac7a
Kajal Pawar <kp668138@gmail.com>
2025-03-12 09:50:39 +0530

    Add program for exception handling with finally keyword

---
+++ ExcepHandling6.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.io.*;
+class ExcepHandling6
+{
+public static void main(String args[])
+{
+int age;
+Scanner sc=new Scanner(System.in);
+
+try
+{
+System.out.println("Enter age:");
+age=sc.nextInt();
+if(age>=18)
+{
+System.out.println("Eligible");
+}
+else
+{
+System.out.println("Not eligible");
+}
+}
+
+catch(Exception e)
+{
+System.out.println(e);
+System.out.println("Please enter valid age");
+}
+
+finally
+{
+System.out.println("Finally block executes");
+}
+
+System.out.println("remaining code");
+
+}
+}
