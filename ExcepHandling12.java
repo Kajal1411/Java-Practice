507cb43379a27b29c25683cc8a006a5abd7c6d5b
Kajal Pawar <kp668138@gmail.com>
2025-03-16 11:17:30 +0530

    Add program for InputMismatchException

---
+++ ExcepHandling12.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+class ExcepHandling12
+{
+public static void main(String args[]) throws InputMismatchException
+{
+int a;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter value for a:");
+a=sc.nextInt();
+System.out.println(a);
+System.out.println("Kajal");
+}
+}
