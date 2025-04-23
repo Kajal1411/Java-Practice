d50677f1f6cdd5d5c31086a6a66b51394785c5b2
Kajal Pawar <kp668138@gmail.com>
2025-04-23 22:47:19 +0530

    Add program for ArrayList to create arraylist using non-generic way

---
+++ ArrayList1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class ArrayList1 
+{
+public static void main(String args[])
+{
+ArrayList al=new ArrayList();
+al.add(40);
+al.add("KDN");
+al.add(1.2);
+al.add('A');
+al.add(50);
+System.out.println(al);
+}
+}
