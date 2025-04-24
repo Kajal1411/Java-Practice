a8bec97e45f868a25c02a4a2531dbe7136d1ef5a
Kajal Pawar <kp668138@gmail.com>
2025-04-25 00:58:14 +0530

    Add program for vector

---
+++ Vector1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Vector1 
+{
+public static void main(String args[])
+{
+Vector v=new Vector();
+v.add(40);
+v.add("KDN");
+v.add(1.2);
+v.add('A');
+v.add(50);
+System.out.println(v);
+
+}
+}
