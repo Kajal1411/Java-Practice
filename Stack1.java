695ae1a324d9667d87459e7587af66608b3d65bc
Kajal Pawar <kp668138@gmail.com>
2025-04-25 01:00:12 +0530

    Add program for Stack

---
+++ Stack1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Stack1
+{
+public static void main(String args[])
+{
+Stack<String> st=new Stack<String>();
+st.push("Deepak");
+st.push("KDN");
+st.push("Infotech");
+st.push("Amit");
+System.out.println(st);
+}
+}
