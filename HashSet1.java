c8eadc865f05e7c29436beee5464bf55c2b4adc7
Kajal Pawar <kp668138@gmail.com>
2025-04-26 23:45:57 +0530

    Add program for hashset

---
+++ HashSet1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class HashSet1
+{
+public static void main(String args[])
+{
+HashSet<String> hs=new HashSet<String>();
+hs.add("Deepak");
+hs.add("KDN");
+hs.add("Infotech");
+hs.add("Amit");
+System.out.println(hs);
+}
+}
