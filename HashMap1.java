066e6b6f13b37392ae8a714ba2ceba1aeeb31296
Kajal Pawar <kp668138@gmail.com>
2025-04-27 00:04:41 +0530

    Add program for HashMap

---
+++ HashMap1.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+class HashMap1
+{
+public static void main(String args[])
+{
+HashMap<Integer,String> hm1=new HashMap<Integer,String>();
+hm1.put(1,"Kajal");
+hm1.put(2,"Vaishnavi");
+hm1.put(3,"Yukta");
+hm1.put(4,"Sakshi");
+hm1.put(5,"Aishwarya");
+System.out.println(hm1);
+}
+}
