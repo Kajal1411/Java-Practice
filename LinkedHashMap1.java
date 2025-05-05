6e53e897d66dc8fe993451cd5c342070fa2b8c8d
Kajal Pawar <kp668138@gmail.com>
2025-05-05 23:50:55 +0530

    Add program for LinkedHashMap

---
+++ LinkedHashMap1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class LinkedHashMap1
+{
+public static void main(String args[])
+{
+LinkedHashMap a=new LinkedHashMap();
+a.put(1,"Kajal");
+a.put(2,"Vaishnavi");
+a.put(3,"Yukta");
+a.put(4,"Sakshi");
+a.put(5,"Aishwarya");
+System.out.println(a);
+}
+}
