72548c42d80ec4321ee7d7a54d6219a530674686
Kajal Pawar <kp668138@gmail.com>
2025-05-05 23:55:05 +0530

    Add program for HashTable

---
+++ HashTable1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class HashTable1
+{
+public static void main(String args[])
+{
+Hashtable ht=new Hashtable();
+ht.put(106,"Kajal");
+ht.put(105,"Vaishnavi");
+ht.put(101,"Yukta");
+ht.put(104,"Sakshi");
+ht.put(102,"Aishwarya");
+System.out.println(ht);
+}
+}
