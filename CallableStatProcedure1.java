83db35721eeb2ad97d9d0854f5a388c35f2798e7
Kajal Pawar <kp668138@gmail.com>
2025-05-13 22:27:51 +0530

    Add program to insert records into table using procedure

---
+++ CallableStatProcedure1.java
@@ -0,0 +1,38 @@
+package MyPack1;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class CallableStatProcedure1 {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","hr","pass");
+		CallableStatement cs=con.prepareCall("{call InsertRecord(?,?)}");
+		
+		cs.setInt(1, 101);
+		cs.setString(2, "Kajal");
+        cs.execute();
+        
+        cs.setInt(1, 102);
+		cs.setString(2, "Vaishnavi");
+        cs.execute();
+        
+        cs.setInt(1, 103);
+		cs.setString(2, "Yukta");
+        cs.execute();
+        
+        cs.setInt(1, 101);
+		cs.setString(2, "Sakshi");
+        cs.execute();
+        
+        cs.setInt(1, 101);
+		cs.setString(2, "Aishwarya");
+        cs.execute();
+        
+        con.close();
+	}
+
+}
