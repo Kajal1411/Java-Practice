10810b49a330cd168b4cbe5d9958f4d9e76dc0a7
Kajal Pawar <kp668138@gmail.com>
2025-05-13 22:29:13 +0530

    Add program to delete records into table using procedure

---
+++ CallableDeleteProcedure.java
@@ -0,0 +1,18 @@
+package MyPack1;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class CallableDeleteProcedure {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","hr","pass");
+		CallableStatement cs=con.prepareCall("{call DeleteRecord(?)}");
+		cs.setInt(1, 103);
+		cs.execute();
+        con.close();
+	}
+}
