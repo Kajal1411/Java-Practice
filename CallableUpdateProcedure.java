2698b427be21750996bcf7d6218c9aadcc4c257d
Kajal Pawar <kp668138@gmail.com>
2025-05-14 10:52:21 +0530

    Add program to update data in database using procedure

---
+++ CallableUpdateProcedure.java
@@ -0,0 +1,21 @@
+package MyPack1;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class CallableUpdateProcedure {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","hr","Pass");
+		CallableStatement cs=con.prepareCall("{call UpdateData(?,?,?)}");
+		cs.setInt(1, 104);
+		cs.setString(2,"xyz");
+		cs.setInt(3, 25000);
+		cs.execute();
+		con.close();
+	}
+
+}
