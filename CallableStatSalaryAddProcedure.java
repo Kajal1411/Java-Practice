22dac184cf48b579a79786a39a779559ba192168
Kajal Pawar <kp668138@gmail.com>
2025-05-14 10:31:06 +0530

    Add program to add two employees salary using procedure

---
+++ CallableStatSalaryAddProcedure.java
@@ -0,0 +1,26 @@
+package MyPack1;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import oracle.jdbc.OracleTypes;
+
+public class CallableStatSararyAddProcedure {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","hr","pass");
+		CallableStatement cs=con.prepareCall("{call AddSalary(?,?,?)}");
+		cs.setInt(1,102);
+		cs.setInt(2,103);
+		cs.registerOutParameter(3, Types.NUMERIC);
+		cs.execute();
+		System.out.println(cs.getInt(3));
+		con.close();
+	}
+
+}
