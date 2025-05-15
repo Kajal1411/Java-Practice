05c600017e772a6c228adaad3f23f71eb0a431fc
Kajal Pawar <kp668138@gmail.com>
2025-05-15 23:51:24 +0530

    Add JDBC program to calculate total salary using function

---
+++ CalableStatTotalSalaryFunction.java
@@ -0,0 +1,21 @@
+package MyPack1;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Types;
+
+public class CallableStatTotalSalaryFunction {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","hr","pass");
+		CallableStatement cs=con.prepareCall("{?=call AddSal()}");
+		cs.registerOutParameter(1, Types.INTEGER);
+		cs.execute();
+		System.out.println(cs.getInt(1));
+		con.close();
+	}
+
+}
