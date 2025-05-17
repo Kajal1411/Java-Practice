b845c72ee267e8e0d08fffca32353d35bfa9cdf8
Kajal Pawar <kp668138@gmail.com>
2025-05-17 09:41:24 +0530

    Update BatchProcessingStat1 program

--- BatchProcessingStat1.java
+++ BatchProcessingStat1.java
@@ -2,32 +2,21 @@ package MyPack1;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class BatchProcessingPreparedStat1 {
+public class BatchProcessingStat1 {
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		 Class.forName("oracle.jdbc.driver.OracleDriver");
-		 Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","hr","pass");
-		 PreparedStatement pst=con.prepareStatement("insert into student values(?,?,?,?,?)");
-		 pst.setInt(1,120);
-		 pst.setInt(2,56);
-		 pst.setInt(3,76);
-		 pst.setInt(4,87);
-		 pst.setInt(5,99);
-		 pst.addBatch();
-		 
-		 pst.setInt(1,121);
-		 pst.setInt(2,55);
-		 pst.setInt(3,77);
-		 pst.setInt(4,44);
-		 pst.setInt(5,97);
-		 pst.addBatch();
-		 
-		 pst.executeBatch();
-		 con.close();
-
+	public static void main(String[] args) throws ClassNotFoundException, SQLException{
+		    Class.forName("oracle.jdbc.driver.OracleDriver");
+		    Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","hr","pass");
+			Statement st=con.createStatement();   
+			st.addBatch("insert into student values(115,47,44,57,45)");
+			st.addBatch("insert into student values(116,62,90,70,70)");
+			st.addBatch("insert into student values(117,87,56,76,35)");
+			st.addBatch("insert into student values(118,45,87,24,86)");
+			st.addBatch("insert into student values(119,56,44,76,45)");
+			st.executeBatch();
+		    con.close();
 	}
-
 }
