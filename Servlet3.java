3258a5e34eee96b670cb81bcb30afedd01afdd2d
Kajal Pawar <kp668138@gmail.com>
2025-05-26 14:33:29 +0530

    Add servlet code

--- Servlet3.java
+++ Servlet3.java
@@ -15,3 +15,59 @@
 </form>
 </body>
 </html>
+
+//Addition.java
+package myPack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Addition extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	ServletConfig config=null;
+	public void init(ServletConfig config)
+	{
+		this.config=config;
+	}
+	
+	public void service(HttpServletRequest request,HttpServletResponse response) throws IOException
+	{
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.print("<html><body>");
+		String n1=request.getParameter("num1");  //getParameter() used to get data from html page
+		String n2=request.getParameter("num2");
+		int a=Integer.parseInt(n1);
+		int b=Integer.parseInt(n2);
+		int c=a+b;
+		out.print("<h1>Sum:"+c+"</h1>");
+		out.print("</body></html>");
+	}
+	
+	public void destroy()
+	{
+		System.out.println("Servlet is destroyed");
+	}
+	
+	public ServletConfig getServletConfig()
+	{
+		return config;		
+	}
+	
+	public String getServletInfo()
+	{
+		return null;
+		
+	}
+
+}
+
+
