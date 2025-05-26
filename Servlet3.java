ee80d12585f5a74c022ba1a2c049d58095da5c5e
Kajal Pawar <kp668138@gmail.com>
2025-05-26 14:34:12 +0530

    Add web.xml file

--- Servlet3.java
+++ Servlet3.java
@@ -71,3 +71,26 @@ public class Addition extends HttpServlet{
 }
 
 
+//web.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<web-app xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://xmlns.jcp.org/xml/ns/javaee" xsi:schemaLocation="http://xmlns.jcp.org/xml/ns/javaee http://xmlns.jcp.org/xml/ns/javaee/web-app_4_0.xsd" id="WebApp_ID" version="4.0">
+  <display-name>DynamicProjAddition</display-name>
+  <welcome-file-list>
+    <welcome-file>index.html</welcome-file>
+    <welcome-file>index.htm</welcome-file>
+    <welcome-file>index.jsp</welcome-file>
+    <welcome-file>default.html</welcome-file>
+    <welcome-file>default.htm</welcome-file>
+    <welcome-file>default.jsp</welcome-file>
+  </welcome-file-list>
+
+<servlet>
+  <servlet-name>DisplayAddition</servlet-name>
+  <servlet-class>myPack.Addition</servlet-class>
+  </servlet>
+  
+  <servlet-mapping>
+   <servlet-name>DisplayAddition</servlet-name>
+   <url-pattern>/DisplayAddition</url-pattern>
+   </servlet-mapping>
+</web-app>
