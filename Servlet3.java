5b4c1d7bf7a4aeb97bea8b594c074956f2205f86
Kajal Pawar <kp668138@gmail.com>
2025-05-26 14:32:04 +0530

    Add program to create Servlet to calculate addition of two numbers and add html page

---
+++ Servlet3.java
@@ -0,0 +1,17 @@
+//Add.html 
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="DisplayAddition">
+<lable for="num1">Enter first number:</lable>
+<input type="number" name="num1"> 
+<lable for="num2">Enter second number:</lable>
+<input type="number" name="num2">
+<button type="submit">Add</button>
+</form>
+</body>
+</html>
