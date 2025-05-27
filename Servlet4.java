e9dc6f41d9b2457f1da23875989ca8793a97afc5
Kajal Pawar <kp668138@gmail.com>
2025-05-27 23:47:01 +0530

    Create Registration page with validation using servlet and add html code here

---
+++ Servlet4.java
@@ -0,0 +1,28 @@
+//index.html
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="registrationServlet">
+<label for="name"><b>Name:</b></label><br>
+<input type="text" name="name"> <br><br>
+    
+<label for="email"><b>Email:</b></label><br>
+<input type="email" name="email"> <br><br>
+
+<label for="mob"><b>Mobile No.:</b></label><br>
+<input type="number" name="mob"> <br><br>
+               
+<label for="address"><b>Address:</label><br>
+<input type="text" name="address"> <br><br>
+                
+<label for="password"><b>Password:</b></label><br>
+<input type="password" name="password">  <br><br>
+               
+<button type="submit">Register</button>
+</form>
+</body>
+</html>
