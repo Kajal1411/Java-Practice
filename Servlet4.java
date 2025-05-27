1b51d5200dc693beaf36c61221115f16ea0606fe
Kajal Pawar <kp668138@gmail.com>
2025-05-27 23:48:32 +0530

    Add web.xml file code

--- Servlet4.java
+++ Servlet4.java
@@ -105,3 +105,28 @@ public void destroy()
 	System.out.println("Servlet is destroy");
 }
 }
+
+
+//Web.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<web-app xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://xmlns.jcp.org/xml/ns/javaee" xsi:schemaLocation="http://xmlns.jcp.org/xml/ns/javaee http://xmlns.jcp.org/xml/ns/javaee/web-app_4_0.xsd" id="WebApp_ID" version="4.0">
+  <display-name>DynamicProjRegistration</display-name>
+  <welcome-file-list>
+    <welcome-file>index.html</welcome-file>
+    <welcome-file>index.htm</welcome-file>
+    <welcome-file>index.jsp</welcome-file>
+    <welcome-file>default.html</welcome-file>
+    <welcome-file>default.htm</welcome-file>
+    <welcome-file>default.jsp</welcome-file>
+  </welcome-file-list>
+  
+  <servlet>
+  <servlet-name>registrationServlet</servlet-name>
+  <servlet-class>myPack.Registration</servlet-class>
+  </servlet>
+  
+  <servlet-mapping>
+  <servlet-name>registrationServlet</servlet-name>
+  <url-pattern>/registrationServlet</url-pattern>
+  </servlet-mapping>
+</web-app>
