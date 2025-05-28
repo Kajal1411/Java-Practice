3e41ba2e28a4cf3e2536ed24981ccf1fff2823b8
Kajal Pawar <kp668138@gmail.com>
2025-05-28 23:06:10 +0530

    Add the Servlet code

--- Servlet5.java
+++ Servlet5.java
@@ -15,3 +15,49 @@
 </form>
 </body>
 </html>
+
+
+//Test.java
+package myPack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Test extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	ServletConfig config;
+	
+	public void init(ServletConfig config)
+	{
+		this.config=config;
+	}
+	
+	public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException
+	{
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.print("<html><body>");
+		String uname=request.getParameter("uname");
+		String upass=request.getParameter("upass");
+		if(uname.equals("kajal")&&upass.equals("1234"))
+		{
+			out.print("Welcone!!!"+uname);
+		}
+		else
+		{
+			out.print("Login fail!!!");
+		}
+		out.print("</body></html>");
+	}
+	
+	public void destroy()
+	{
+		System.out.println("Servlet destroyed!!!");
+	}
+
+}
