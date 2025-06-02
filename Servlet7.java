d9fafa6a49fae60ef91e675d5bc1a613c605e561
Kajal Pawar <kp668138@gmail.com>
2025-06-02 14:44:37 +0530

    Startig creating the Program for java RequestDispatcher interface. here, add login.html page

---
+++ Servlet7.java
@@ -0,0 +1,18 @@
+//login.html
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="first">
+<label for="name"><b>Username:</b></label><br>
+<input type="text" name="name"> <br><br>
+<label for="password"><b>Password:</b></label><br>
+<input type="password" name="pass"> 
+<br><br>
+<button type="submit">Login</button>
+</form>
+</body>
+</html>
