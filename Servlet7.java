e73d22722023ac1575c4a2c516e7a679c52e88b2
Kajal Pawar <kp668138@gmail.com>
2025-06-02 14:48:28 +0530

    Add servlet code

--- Servlet7.java
+++ Servlet7.java
@@ -54,3 +54,64 @@
 <a href="contactus.html">ContactUs</a>
 </body>
 </html>
+
+//Test.java
+package myPack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Test extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	ServletConfig config=null;
+	public void init(ServletConfig config)
+	{
+		this.config=config;
+	}
+	
+	public void service(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException
+	{
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.print("<html><body>");
+		String uname=request.getParameter("name");
+		String upass=request.getParameter("pass");
+		if(uname.equals("kajal") && upass.equals("1234"))
+		{
+			RequestDispatcher rd=request.getRequestDispatcher("/welcome.html");
+			rd.forward(request, response);
+		}
+		else
+		{
+			out.println("<h1>Invalid Username or Password</h1>");
+			RequestDispatcher rd=request.getRequestDispatcher("/login.html");
+			rd.include(request, response);
+		}
+		out.print("</body></html>");
+	}
+	
+	public void destroy()
+	{
+		System.out.println("Servlet is destroyed");
+	}
+	
+	public ServletConfig getServletConfig()
+	{
+		return config;		
+	}
+	
+	public String getServletInfo()
+	{
+		return null;
+		
+	}
+
+
+}
