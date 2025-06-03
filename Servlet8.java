cde277ef367090a04674e70912420352315b87df
Kajal Pawar <kp668138@gmail.com>
2025-06-03 09:41:58 +0530

    Starting creating the Servlet for sendRedirect() method And here, add the HTML code

---
+++ Servlet8.java
@@ -0,0 +1,17 @@
+//sendRedirect() method
+
+//Demo.html
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="first">
+<label for="data">Type something which want to search:</label>
+<input type="text" name="data">
+<button type="submit">Search</button>
+</form>
+</body>
+</html>
