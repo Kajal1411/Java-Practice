5e44ef914df47f57919c170cb69a501a72bac330
Kajal Pawar <kp668138@gmail.com>
2025-06-03 09:42:41 +0530

    Add the servlet code

--- Servlet8.java
+++ Servlet8.java
@@ -15,3 +15,42 @@
 </form>
 </body>
 </html>
+
+//Test.java
+package myPack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Test extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	ServletConfig config=null;
+	public void init(ServletConfig config)
+	{
+		this.config=config;
+	}
+	
+	public void service(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException
+	{
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.print("<html><body>");
+		String data=request.getParameter("data");
+		response.sendRedirect("https://www."+data+".com");		
+		out.print("</body></html>");
+	}
+	
+	public void destroy()
+	{
+		System.out.println("Servlet is destroyed");
+	}
+	
+
+}
