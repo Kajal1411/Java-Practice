f9ce19cbea5d0d101f3f45154cd44a3fc3529ce0
Kajal Pawar <kp668138@gmail.com>
2025-06-06 10:18:55 +0530

    Starting creating program to implement the concept of HttpSession and here add the login.html page

---
+++ Servlet11.java
@@ -0,0 +1,20 @@
+//HttpSession
+
+//login.html
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="first" method="get">
+<label for="name"><b>Username:</b></label><br>
+<input type="text" name="name"> <br><br>
+<label for="pass"><b>Password:</b></label><br>
+<input type="password" name="pass"> 
+<br><br>
+<button type="submit">Login</button><br><br><br>
+</form>
+</body>
+</html>
