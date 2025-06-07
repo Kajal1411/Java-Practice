9c34c55aea36199b47ac9884c5f19532b6fb4cc7
Kajal Pawar <kp668138@gmail.com>
2025-06-07 19:33:16 +0530

    Add first servlet

--- Servlet12.java
+++ Servlet12.java
@@ -18,3 +18,42 @@
 </form>
 </body>
 </html>
+
+//Servet1.java
+package myPack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Test1 extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
+	{
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.print("<html><body>");
+		String name=request.getParameter("name");	
+		String pass=request.getParameter("pass");	
+		out.print("Username:"+name);
+		out.print("Password:"+pass);
+		out.print("<a href='Test2'>Click</a>");
+		
+		Cookie ck=new Cookie("uname",name);
+		response.addCookie(ck);
+		ck=new Cookie("upass",pass);
+		response.addCookie(ck);
+		
+		out.print("</body></html>");
+		
+	}
+
+}
