8a4327e5f6e5483bfbf757f936e804018026baed
Kajal Pawar <kp668138@gmail.com>
2025-06-09 00:05:12 +0530

    Starting creating program for HiddenFormField session managenment technique and here add the login.html page

---
+++ Servlet14.java
@@ -0,0 +1,17 @@
+//HiddenFormField
+
+//login.html
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="first" method="get">
+<lable for="name">Enter your name:</lable>
+<input type="text" name="name"><br><br>
+<button type="submit">Submit</button>
+</form>
+</body>
+</html>
