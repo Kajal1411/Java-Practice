e3be8a9943c658d8c9a2306b689c1a2ca7de6238
Kajal Pawar <kp668138@gmail.com>
2025-06-08 18:21:12 +0530

    Add first servlet code

--- Servlet13.java
+++ Servlet13.java
@@ -16,4 +16,33 @@
 </body>
 </html>
 
+//Test1.java
+package myPack;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Test1 extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException
+	{
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String name=request.getParameter("name");
+		out.print("<html><body>");
+		out.print("Wecome"+name+"<br>");
+		//Appending name in query String
+		out.print("<a href='Test2?name="+name+"'>AboutUs</a>");
+		out.print("</body></html>");	
+	}
+
+}
