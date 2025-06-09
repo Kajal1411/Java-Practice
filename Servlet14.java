09bd6f2fbbd6ba06f8f51cd15c6078d5c549e5e7
Kajal Pawar <kp668138@gmail.com>
2025-06-09 13:07:49 +0530

    Add first servlet code

--- Servlet14.java
+++ Servlet14.java
@@ -15,3 +15,42 @@
 </form>
 </body>
 </html>
+
+
+//Test1.java
+package myPack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Test1 extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException
+	{
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String name=request.getParameter("name");
+		out.print("<html><body>");
+		out.print("Wecome"+name+"<br>");
+		
+		//Creating form that have invisible text field
+		
+		out.print("<form action='Test2'>");
+		out.print("<input type='hidden' name='name' value="+name+">");
+		out.print("<button type='submit'>Submit</button>");
+		out.print("</form>");
+		
+		
+		out.print("</body></html>");	
+	}
+
+
+}
