b42c4dbcec91c8d0df6dd2bffc5a7e8ca595562b
Kajal Pawar <kp668138@gmail.com>
2025-06-10 18:05:17 +0530

    Add prgram for ServletContextEvent and here add the Demo.html code

---
+++ Servlet15.java
@@ -0,0 +1,15 @@
+//ServletContextEvent
+
+//Demo.html
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="MyServlet" method="get">
+<button type="submit">Next</button>
+</form>
+</body>
+</html>
