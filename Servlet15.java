6cdb3adf95631b8ae50c138560b0cac64fc0f268
Kajal Pawar <kp668138@gmail.com>
2025-06-10 18:07:10 +0530

    Add event handling code

--- Servlet15.java
+++ Servlet15.java
@@ -82,7 +82,49 @@ public class MyServlet extends HttpServlet{
 		out.print("</body></html>");
 		
 	}
-	
+}
 
+
+//MyServletContextListener.java
+package myPack;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class MyServletContextListner implements ServletContextListener{
+
+	public void  contextInitialized(ServletContextEvent event)
+	{
+		ServletContext context=event.getServletContext();
+		System.out.println("Servlet Context Initialized");
+		
+		//Performs Initialization task here
+		String url=context.getInitParameter("url");
+		String username=context.getInitParameter("username");
+		String password=context.getInitParameter("password");
+		
+		//Store DB Connection setting in servletContext
+		context.setAttribute("url", url);
+		context.setAttribute("username", username);
+		context.setAttribute("password", password);
+		
+	}
+	
+	public void contextDestroyed(ServletContextEvent event)
+	{
+		    ServletContext context = event.getServletContext();
+
+	        // Perform cleanup tasks here
+	        context.removeAttribute("url");
+	        context.removeAttribute("username");
+	        context.removeAttribute("password");
+
+	        System.out.println("ServletContext destroyed");
+	        System.out.println("Database connection settings cleaned up");
+	}
+
+	
+	
 
 }
