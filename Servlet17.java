cff60c0eb54f2be44250f16d78803603ca414168
Kajal Pawar <kp668138@gmail.com>
2025-06-12 00:20:32 +0530

    Add logout servlet code

--- Servlet17.java
+++ Servlet17.java
@@ -87,3 +87,42 @@ public class Event implements HttpSessionListener{
     }  
 
 }
+
+
+//logout.java
+package myPack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class Logout extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	
+	private static final long serialVersionUID = 1L;
+	
+	public void doGet(HttpServletRequest request,  HttpServletResponse response) throws IOException, ServletException
+	{
+		response.setContentType("text/html");  
+        PrintWriter out = response.getWriter();  
+      
+        HttpSession session=request.getSession(false);  
+        session.invalidate();//invalidating session  
+          
+        out.print("You are successfully logged out");   
+        
+        RequestDispatcher rd=request.getRequestDispatcher("/index.html");
+		rd.include(request, response);
+	}
+
+
+}
