6cd49455fa6b68fb71cf0e8b5b3099b109011933
Kajal Pawar <kp668138@gmail.com>
2025-06-12 00:17:06 +0530

    Add program HttpSessionEvent and here add the index.html file

---
+++ Servlet17.java
@@ -0,0 +1,18 @@
+//index.html
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="Test">
+<label for="name"><b>Username:</b></label><br>
+<input type="text" name="name"> <br><br>
+<label for="password"><b>Password:</b></label><br>
+<input type="password" name="password"> 
+<br><br>
+<button type="submit">Login</button><br><br><br>
+</form>
+</body>
+</html>
