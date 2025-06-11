c77c158e33693bedce6299a88db85615e65dfc9f
Kajal Pawar <kp668138@gmail.com>
2025-06-11 10:29:37 +0530

    Add servlet code

--- Servlet16.java
+++ Servlet16.java
@@ -47,3 +47,41 @@ public class MyListnerCount implements  HttpSessionListener{
 	}
 
 }
+
+
+//ActiveUserCount.java
+package myPack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class ActiveUserCount extends HttpServlet{
+	
+	private Object activeUsers;
+
+	protected void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException
+	{
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.print("<html><body>");
+		String name=request.getParameter("name");
+		out.print("Welcome "+name);
+		out.print("</body></html>");
+		
+		HttpSession session=request.getSession();
+		session.setAttribute("name", name);
+		
+		int activeUsers=MyListnerCount.getActiveUsers();
+		request.setAttribute("activeUsers", activeUsers);
+		RequestDispatcher rd=request.getRequestDispatcher("/index.jsp");
+		rd.include(request, response);
+	}
+
+}
