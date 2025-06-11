069cbefa87d9293de887124c87b1c2a7a5c0b49c
Kajal Pawar <kp668138@gmail.com>
2025-06-12 00:18:54 +0530

    Add servlet code

--- Servlet17.java
+++ Servlet17.java
@@ -16,3 +16,44 @@
 </form>
 </body>
 </html>
+
+//Test.java
+package myPack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class Test extends HttpServlet{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest request,  HttpServletResponse response) throws IOException  
+	{
+		response.setContentType("text/html");  
+        PrintWriter out = response.getWriter();  
+      
+        String name=request.getParameter("name");  
+        out.print("Welcome "+name);  
+          
+        HttpSession session=request.getSession();  
+        session.setAttribute("name",name);  
+          
+      //retrieving data from ServletContext object  
+        ServletContext context=getServletContext();  
+        int totalusers=(Integer)context.getAttribute("totalusers");  
+        int currentusers=(Integer)context.getAttribute("currentusers");  
+        out.print("<br>total users= "+totalusers);  
+        out.print("<br>current users= "+currentusers);  
+        out.print("<br><a href='Logout'>logout</a>");  
+	}
+
+}
