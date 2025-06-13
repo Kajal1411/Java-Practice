fadbd762acf5c5c39d8d249b7c7960fdf6df20eb
Kajal Pawar <kp668138@gmail.com>
2025-06-13 10:23:50 +0530

    Starting creating a smmall web application using servlet and here add the. Registration.html file

---
+++ ServletAplication.java
@@ -0,0 +1,23 @@
+//Registration.html
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="first">
+<label for="fn"><b>First Name:</b></label> <br>
+<input type="text" name="fn"> <br><br>
+<label for="ln"><b>Last Name:</b></label> <br>
+<input type="text" name="ln"> <br><br>
+<label for="mob"><b>Mobile Number:</b></label> <br>
+<input type="number" name="mob"> <br><br>
+<label for="uname"><b>UserName:</b></label> <br>
+<input type="text" name="uname"> <br><br>
+<label for="upass"><b>Password:</b></label> <br>
+<input type="password" name="upass"> <br><br>
+<button type="submit">Register</button>
+</form>
+</body>
+</html>
