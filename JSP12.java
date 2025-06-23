4977b6464fe9c2614225c8173264b7de238e8af0
Kajal Pawar <kp668138@gmail.com>
2025-06-23 21:07:14 +0530

    Add Program for Java Bean class to take the username and password from  user and display it using Java Bean class

---
+++ JSP12.java
@@ -0,0 +1,85 @@
+//login.html
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>User Form</title>
+</head>
+<body>
+
+<form action="BeanClass.jsp" method="get">
+    <label for="name"><b>Name:</b></label><br>
+    <input type="text" name="name" required><br><br>
+
+    <label for="email"><b>Email:</b></label><br>
+    <input type="email" name="email" required><br><br>
+
+    <button type="submit">Submit</button>
+</form>
+
+</body>
+</html>
+
+
+
+//Test.java
+package myPack;
+
+import java.io.Serializable;
+
+public class Test implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+    private String email;
+
+    public Test() 
+    {
+    	
+    }
+
+    public String getName() 
+    {
+        return name;
+    }
+
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+
+    public String getEmail() 
+    {
+        return email;
+    }
+
+    public void setEmail(String email) 
+    {
+        this.email = email;
+    }
+}
+
+
+
+//BeanClass.jsp
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%@ page import="myPack.Test" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>User Details</title>
+</head>
+<body>
+
+<jsp:useBean id="u" class="myPack.Test" scope="page" />
+<jsp:setProperty property="*" name="u" />
+
+<h2>User Details:</h2>
+
+Name:      <jsp:getProperty property="name" name="u" /></p>
+Email:     <jsp:getProperty property="email" name="u" /></p>
+
+</body>
+</html>
