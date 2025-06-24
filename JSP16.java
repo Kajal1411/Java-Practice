407a8c3d02fefab19a73e823ec6574571ddd4f3b
Kajal Pawar <kp668138@gmail.com>
2025-06-24 19:50:22 +0530

    Add program to print username and password using Expression laguage and application scope

---
+++ JSP16.java
@@ -0,0 +1,41 @@
+//login.html
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="welcome.jsp" method="get">
+    <label for="name"><b>Name:</b></label><br>
+    <input type="text" name="name" required><br><br>
+
+    <label for="email"><b>Email:</b></label><br>
+    <input type="email" name="email" required><br><br>
+
+    <button type="submit">Submit</button>
+</form>
+</body>
+</html>
+
+
+//welcome.jsp
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<% 
+String name=request.getParameter("name");
+String email=request.getParameter("email");
+application.setAttribute("name",name);
+application.setAttribute("email",email);
+%>
+Name:   ${applicationScope.name}
+Email:  ${applicationScope.email}
+</body>
+</html>
