e6bf549a0d6879f034b7247daddf89c00c0f662d
Kajal Pawar <kp668138@gmail.com>
2025-06-25 19:28:55 +0530

    Add program for JSP EL operators

---
+++ JSP18.java
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+Addition: ${5+2}
+<br><br>
+Substraction: ${5-1 }
+<br><br>
+Multiplication: ${ 5*2}
+<br><br>
+Division: ${ 10/2}
+<br><br>
+Modulo: ${10%3}
+<br><br>
+equal to: ${5==5}
+<br><br>
+equal to: ${5==4}
+<br><br>
+not equal to ${5!=4}
+<br><br>
+not equal to ${5!=5}
+
+</body>
+</html>
