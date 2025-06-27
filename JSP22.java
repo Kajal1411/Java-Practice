56c31e7ff4d56f140c7adf8ea26fcfd617162e2c
Kajal Pawar <kp668138@gmail.com>
2025-06-27 12:47:24 +0530

    Add tld file

--- JSP22.java
+++ JSP22.java
@@ -251,3 +251,29 @@ String userid=request.getParameter("userid");
 </form>
 </body>
 </html>
+
+
+//myTag.tld
+<?xml version="1.0" encoding="ISO-8859-1" ?>  
+<!DOCTYPE taglib  
+        PUBLIC "-//Sun Microsystems, Inc.//DTD JSP Tag Library 1.2//EN"  
+        "http://java.sun.com/j2ee/dtd/web-jsptaglibrary_1_2.dtd">  
+  
+<taglib>  
+  <tlib-version>1.0</tlib-version>  
+  <jsp-version>1.2</jsp-version>  
+  <short-name>simple</short-name>  
+  <uri>http://tomcat.apache.org/example-taglib</uri>  
+  <description>A simple tab library for the examples</description>  
+  
+  <tag>  
+    <name>history</name>  
+    <tag-class>myPack.Test</tag-class>  
+    <attribute>  
+    <name>userid</name>  
+    <required>true</required>  
+    <rtexprvalue>true</rtexprvalue>
+    </attribute>  
+  </tag>  
+  
+</taglib>  
